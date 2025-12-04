63665ce86fe84fd918648d32e7cf4e66c475a12d
ZERIXA3 <lukahadzic1@gmail.com>
2025-12-04 15:06:13 +0100

    Failed insertions in test table with Clojure

--- src/clojure_application_project/db.clj
+++ src/clojure_application_project/db.clj
@@ -1,5 +1,5 @@
 (ns clojure-application-project.db
-  (:require [next.jdbc :as jdbc]))
+  (:require [next.jdbc :as jdbc] ))
 
 (def dbSpecs {
               :dbtype "mysql"
@@ -23,3 +23,53 @@
                  skill INT NOT NULL,
                  idT INT,
                  FOREIGN KEY(idT) REFERENCES teams(idT))"]))
+
+(defn initTestTable []
+  (jdbc/execute! dbSpecs
+                 ["CREATE TABLE IF NOT EXISTS testTable
+                 (idTt INT AUTO_INCREMENT PRIMARY KEY,
+                 cntVal INT,
+                 sumVal INT)"]))
+
+(defn alterTestTable []
+  (jdbc/execute! dbSpecs
+                 ["ALTER TABLE testTable
+                 ADD COLUMN idT INT"]))
+
+;(defn insertIntoTestTable []
+;
+;  (let [rows [[1 3 2 1]
+;              [2 4 3 2]
+;              [3 7 1 1]]]
+
+   ;(jdbc/execute! dbSpecs
+   ;               ["INSERT INTO testtable (idTt, cntVal, sumVal, idT)
+   ;              VALUES(?, ?, ?, ?)"] {:multi? true :param rows})))
+
+;(defn insertIntoTestTable []
+;  (let [rows [{:inTt 1 :cntVal 3 :sumVal 2 :idT 1}
+;              {:inTt 2 :cntVal 4 :sumVal 3 :idT 2}
+;              {:inTt 3 :cntVal 7 :sumVal 1 :idT 1}]]
+;    (jdbc/execute! dbSpecs
+;                   ["INSERT INTO testTable (idTt, cntVal, sumVal, idT) VALUES (:idTt, :cntVal, :sumVal, :idT)"]
+;                   {:multi? true
+;                    :params rows})))
+
+
+
+;(defn insertIntoTestTable []
+;
+;  (let [rows [[1 3 2 1]
+;              [2 4 3 2]
+;              [3 7 1 1]]]
+;
+;(batch/execute-batch! (jdbc/get-connection dbSpecs)
+;               ["INSERT INTO testtable (idTt, cntVal, sumVal, idT)
+;              VALUES(?, ?, ?, ?)"] {:multi? true :param rows})))
+
+;(defn insertIntoTestTable []
+;  (jdbc/execute! dbSpecs ["INSERT INTO testTable (idTt, cntVal, sumVal, idT)
+;                        VALUES(?, ?, ?, ?)"][1 3 2 1])
+;  )
+
+
